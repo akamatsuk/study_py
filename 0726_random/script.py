5e7bbcaf55f1159cc3953ea432165959f8ca2fbd
Koki Akamatsu <list6311@gmail.com>
2020-07-26 14:25:22 +0900

    add human

--- 0726_random/script.py
+++ 0726_random/script.py
@@ -1,4 +1,4 @@
 import random
-humans = ['aka', 'yas', 'tak', 'shope', 'atu']
+humans = ['aka', 'yas', 'tak', 'shope', 'atu', 'ari']
 select = random.randint(0,len(humans)-1)
 print('今回選ばれたのは' + humans[select] + 'さんです')
