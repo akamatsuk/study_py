24c5b4f4ea8f9e34a34179b14f798dbb4dd7127b
Koki Akamatsu <list6311@gmail.com>
2020-07-27 16:11:03 +0900

    add entrant

--- 0726_random/script.py
+++ 0726_random/script.py
@@ -1,4 +1,11 @@
 import random
-humans = ['aka', 'yas', 'tak', 'shope', 'atu']
-select = random.randint(0,len(humans)-1)
+entrant = int(input('参加者は何名ですか？：'))
+humans = {1:'you'}
+# 辞書の追加と参加人数が合うようにする
+humans[1] = str(input('あなたの名前：'))
+x = 2
+while  x <= entrant:
+    humans[x] = str(input('参加者'+ str(x-1) +'人目の名前：'))
+    x += 1
+select = random.randint(1,len(humans))
 print('今回選ばれたのは' + humans[select] + 'さんです')
