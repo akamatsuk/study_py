af4b0f04ac8cb4b58ed1cc442ce6b274d2a1ff56
Koki Akamatsu <list6311@gmail.com>
2020-07-30 01:49:54 +0900

    comment out

--- 0728/train_a_classifier.py
+++ 0728/train_a_classifier.py
@@ -1,6 +1,6 @@
 # 2. train a classifier
 import numpy as np
-#import graphviz
+import graphviz
 from sklearn.datasets import load_iris
 from sklearn import tree
 iris = load_iris()
@@ -19,10 +19,10 @@ clf.fit(train_data, train_target)
 print(test_target)
 print(clf.predict(test_data))
 # 4. visualize the tree
-#dot_data = tree.export_graphviz(clf, out_file=None,
- #                               feature_names=iris.feature_names,
-  #                              class_names=iris.target_names,
-   #                             filled=True, rounded=True,
-    #                            special_characters=True)
-#graph = graphviz.Source(dot_data)
-#graph.render("iris")
+dot_data = tree.export_graphviz(clf, out_file=None,
+                                feature_names=iris.feature_names,
+                                class_names=iris.target_names,
+                                filled=True, rounded=True,
+                                special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render("iris")
